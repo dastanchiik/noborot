648d4c1052c18833affdf589a563119aa5b4dc30
dastanchick <dosyafree32@gmail.com>
2022-11-13 20:04:44 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+    int[]array ={1,2,3,4,5,6,7,8,9,0};
+        for (int i = array.length -1; i >=0 ; i--) {
+            System.out.print(array[i]+" ");
+
+        }
+    }
+}
